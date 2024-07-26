ea53cd68fba161a18555a4bff94d166ae44f0305
akane-tsutsui <akane-tsutsui@mole-inc.co>
2024-07-26 11:01:57 +0900

    テスト

--- src/pages/parts/Top/Appeal/index.tsx
+++ src/pages/parts/Top/Appeal/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 const Appeal = () => {
   return (
     <section className={styles.appeal}>
-      <p className={styles.appeal__p}>問題解決 できるデザインを作る</p>
+      <p className={styles.appeal__p}>問題解決</p>
     </section>
   );
 };
